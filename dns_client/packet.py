d3b2fc3b7119dda84aaa67e44528c477aa0223e2
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-09-25 16:01:51 -0400

    feat: full flow mostly complete

--- dns_client/packet.py
+++ dns_client/packet.py
@@ -1,5 +1,6 @@
 import random
 import struct
+from types import prepare_class
 from typing import Optional
 
 
@@ -45,35 +46,33 @@ class PacketHeader:
         )
 
     @classmethod
-    def build_request_header(cls, question_count: int):
-        header = cls(
+    def build_request(cls, question_count: int):
+        return cls(
             id=cls.__generate_id(),
             recursive=True,
             question_count=question_count,
         )
 
-        return header
-
     @classmethod
-    def build_response_header(cls, raw: bytes):
-        id, flag, qcount, acount, nscount, arcount = struct.unpack("!HHHHHH", raw)
-        header = PacketHeader(
+    def build_response(cls, header: bytes):
+        id, flag, qcount, acount, nscount, arcount = struct.unpack("!HHHHHH", header)
+        h = PacketHeader(
             id=id,
             question_count=qcount,
             answer_count=acount,
             nameserver_records_count=nscount,
             additional_records_count=arcount,
         )
-        header.__unpack_flag(flag)
-        return header
+        h.__unpack_flag(flag)
+        return h
 
     def __pack_flag(self) -> int:
-        qr = self.to_bit(self.response)
+        qr = self.__to_bit(self.response)
         opcode = self.opcode
-        aa = self.to_bit(self.authoritative)
-        tc = self.to_bit(self.truncated)
-        rd = self.to_bit(self.recursive)
-        ra = self.to_bit(self.recursive_supported)
+        aa = self.__to_bit(self.authoritative)
+        tc = self.__to_bit(self.truncated)
+        rd = self.__to_bit(self.recursive)
+        ra = self.__to_bit(self.recursive_supported)
         z = self.z
         rcode = self.response_code
         flag = (
@@ -102,21 +101,21 @@ class PacketHeader:
         z = mask(4, 0x07)
         rcode = mask(0, 0x0F)
 
-        self.response = self.from_bit(qr)
+        self.response = self.__from_bit(qr)
         self.opcode = opcode
-        self.authoritative = self.from_bit(aa)
-        self.truncated = self.from_bit(tc)
-        self.recursive = self.from_bit(rd)
-        self.recursive_supported = self.from_bit(ra)
+        self.authoritative = self.__from_bit(aa)
+        self.truncated = self.__from_bit(tc)
+        self.recursive = self.__from_bit(rd)
+        self.recursive_supported = self.__from_bit(ra)
         self.z = z
         self.response_code = rcode
 
     @staticmethod
-    def to_bit(b: bool) -> int:
+    def __to_bit(b: bool) -> int:
         return 1 if b else 0
 
     @staticmethod
-    def from_bit(b: int) -> bool:
+    def __from_bit(b: int) -> bool:
         return False if b == 0 else True
 
     @staticmethod
@@ -124,10 +123,14 @@ class PacketHeader:
         return random.randint(0, 65535)
 
 
-class PacketQuestion:
+class RecordType:
     A = 0x0001
     NS = 0x0002
     MX = 0x000F
+    CNAME = 0x0005
+
+
+class PacketQuestion:
     QCLASS = 0x0001
 
     def __init__(self, name: str, mx: Optional[bool] = None, ns: Optional[bool] = None):
@@ -138,11 +141,11 @@ class PacketQuestion:
     def pack(self) -> bytes:
         qname = self.__pack_host_name()
         if self.mx:
-            qtype = self.MX
+            qtype = RecordType.MX
         elif self.ns:
-            qtype = self.NS
+            qtype = RecordType.NS
         else:
-            qtype = self.A
+            qtype = RecordType.A
 
         packet = qname + struct.pack("!HH", qtype, self.QCLASS)
         return packet
@@ -162,22 +165,117 @@ class PacketQuestion:
         return b"".join(parts)
 
     @classmethod
-    def build_question(cls, raw: bytes):
-        data = raw[:-4]
-        # TODO: decode name
+    def build_question(cls, question: bytes):
+        data = question[:-4]
         name = data.decode()
 
-        end = raw[-4:]
+        end = question[-4:]
         qtype, _ = struct.unpack("HH", end)
-        mx = qtype == cls.MX
-        ns = qtype == cls.NS
+        mx = qtype == RecordType.MX
+        ns = qtype == RecordType.NS
 
         return PacketQuestion(name=name, mx=mx, ns=ns)
 
 
 class PacketAnwser:
-    def pack(self) -> bytes:
-        return b""
+    def __init__(
+        self,
+        name: str,
+        data_type: int,
+        clazz: int,
+        ttl: int,
+        data_length: int,
+        data: bytes,
+        preference: int,
+        exchange: str,
+    ):
+        self.name: str = name
+        self.data_type: int = data_type
+        self.clazz: int = clazz
+        self.ttl: int = ttl
+        self.data_length: int = data_length
+        self.data: bytes = data
+        self.preference: int = preference
+        self.exchange: str = exchange
+
+    @classmethod
+    def build_answer(
+        cls, response: bytes, pointer: int, header: PacketHeader
+    ) -> "list[PacketAnwser] | None":
+        answers = []
+        for answer_idx in range(header.answer_count):
+            name, pointer = cls.__extract_name(response, pointer)
+            data_start = pointer + 10
+            meta = response[pointer:data_start]
+            data_type, clazz, ttl, data_length = struct.unpack("!HHIH", meta)
+
+            preference_start = data_start + data_length
+            rdata = response[data_start:preference_start]
+            # TODO: check the type and decode it
+
+            exchange_start = preference_start + 2
+            (preference,) = struct.unpack(
+                "!H", response[preference_start:exchange_start]
+            )
+
+            exchange, pointer = cls.__extract_name(response, exchange_start)
+
+            a = PacketAnwser(
+                name=name,
+                data_type=data_type,
+                clazz=clazz,
+                ttl=ttl,
+                data_length=data_length,
+                data=rdata,
+                preference=preference,
+                exchange=exchange,
+            )
+
+            print(
+                name,
+                data_type,
+                clazz,
+                ttl,
+                data_length,
+                rdata,
+                preference,
+                exchange,
+            )
+
+            answers.append(a)
+
+        return answers
+
+    @classmethod
+    def __extract_name(cls, response: bytes, start: int) -> tuple[str, int]:
+        labels = []
+        out_pointer = start + 1
+        while True:
+            length = response[start]
+            if length == 0:
+                break
+
+            if cls.__is_label_pointer(length):
+                pointer = cls.__decode_pointer(length, response[start + 1])
+                out_pointer = start + 2
+                start = pointer
+                continue
+
+            label_start = start + 1
+            label = response[label_start : label_start + length]
+            label_str = label.decode("ascii")
+            labels.append(label_str)
+            start = label_start + length
+
+        return (".".join(labels), out_pointer)
+
+    @staticmethod
+    def __is_label_pointer(first_byte: int) -> bool:
+        return first_byte & 0b11000000 == 0b11000000
+
+    @staticmethod
+    def __decode_pointer(first_byte: int, second_byte: int) -> int:
+        return ((first_byte & 0x3F) << 8) + second_byte
 
 
 class Packet:
@@ -185,35 +283,30 @@ class Packet:
         self,
         header: PacketHeader,
         question: PacketQuestion,
-        answer: Optional[PacketAnwser] = None,
+        answers: Optional[list[PacketAnwser]] = None,
     ):
         self.header = header
         self.question = question
-        self.answer = answer
+        self.answers = answers
 
     def pack(self) -> bytes:
-        parts = [self.header.pack(), self.question.pack()]
-        if self.answer:
-            parts.append(self.answer.pack())
-
-        return b"".join(parts)
+        return b"".join([self.header.pack(), self.question.pack()])
 
     @staticmethod
-    def build_request_packet(
+    def build_request(
         name: str,
         mx: Optional[bool] = None,
         ns: Optional[bool] = None,
-    ):
-        header = PacketHeader.build_request_header(1)
+    ) -> "Packet":
+        header = PacketHeader.build_request(1)
         question = PacketQuestion(name, mx, ns)
         return Packet(header=header, question=question)
 
     @staticmethod
-    def build_response_packet(response: bytes, question: PacketQuestion):
-        header_cutoff = 12
-        header = PacketHeader.build_response_header(response[:header_cutoff])
-        q = PacketQuestion.build_question(
-            response[header_cutoff : header_cutoff + question.get_packed_length()]
-        )
-
-        return Packet(header=header, question=q)
+    def build_response(raw: bytes, request: "Packet") -> "Packet":
+        question_pointer = 12
+        answer_pointer = question_pointer + request.question.get_packed_length()
+        header = PacketHeader.build_response(raw[:question_pointer])
+        q = PacketQuestion.build_question(raw[question_pointer:answer_pointer])
+        answers = PacketAnwser.build_answer(raw, answer_pointer, header)
+        return Packet(header=header, question=q, answers=answers)
--- dns_client/transmission.py
+++ dns_client/transmission.py
@@ -1,7 +1,7 @@
 import socket
 
 from configuration import Configuration
-from packet import Packet, PacketHeader, PacketQuestion
+from packet import Packet
 
 
 class Transmitter:
@@ -9,18 +9,36 @@ class Transmitter:
         self.config = config
 
     def transmit(self):
+        print("connecting to dns server: ", self.config.server)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.connect((self.config.server, self.config.port))
         sock.settimeout(self.config.timeout)
-        request = Packet.build_request_packet(
+
+        print("querying for hostname: ", self.config.name)
+        request = Packet.build_request(
             self.config.name, mx=self.config.mx, ns=self.config.ns
         )
-        packet = request.pack()
 
-        print("Sending: ", packet)
+        print("packing request...")
+        packet = request.pack()
+        print("packed: ", packet)
         sock.send(packet)
         res = sock.recv(512)
-        print("Recieved: ", res)
+        print("recieved packet: ", res)
 
-        response = Packet.build_response_packet(res, request.question)
-        print("Built response packet: ", response.question.name)
+        print("unpacking response...")
+        response = Packet.build_response(res, request)
+        print("answer count in response: ", response.header.answer_count)
+        print("response code", response.header.response_code)
+        print("recusive supported: ", response.header.recursive_supported)
+
+        if response.answers:
+            for answer in response.answers:
+                print("answer:")
+                print("hostname: ", answer.name)
+                print("type: ", answer.data_type)
+                print("class: ", answer.clazz)
+                print("data length: ", answer.data_length)
+                print("data: ", answer.data)
+                print("preference: ", answer.preference)
+                print("exchange (mail): ", answer.exchange)
